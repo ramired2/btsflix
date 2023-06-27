f9d9d32c7928328def7d257911589cb111067e53
Daisy <ramired2@oregonstate.edu>
2023-06-26 22:02:03 -0700

    before video selects complete 1/2 wrking video

--- DML.sql
+++ DML.sql
@@ -8,17 +8,18 @@
         INSERT INTO members(koreanName, lastName, firstName, alias)
 			VALUES	(:kn , :ln, :fn, :alias);
 
-	-- SELECT
+	-- SELECT -- unless specified order by their fanchant 
 		-- select all members
+        SELECT * FROM Members;
         
+        -- select all members w out other known aliases (like suga, agustD, prodD)
+		SELECT Members.memberID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias FROM Members GROUP BY fullName;
         
-		-- select members by specified last name
-       
-       
-        -- select members by specified first name
-        
+		-- select members by specified last name OR first name (w out other known aliases)
+       SELECT Members.memberID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName , Members.alias FROM Members WHERE (Members.lastName = "" OR Members.firstName = "") GROUP BY fullName;
         
         -- select members by ID
+        SELECT Members.memberID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias FROM Members WHERE Members.memberID = :fromDropdown
         
         -- select all people starring in a video
         SELECT Starring.videoID, Starring.artistID, Members.memberID, Members.alias, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.lastName as ln
@@ -28,12 +29,9 @@
             WHERE Videos.videoID = :videoID
             ORDER BY Members.lastName ASC;
             
-        
-            
 	-- UPDATE
 		UPDATE members SET koreanName = :kn, lastName = :ln, firstName = :fn, alias = :alias WHERE memberID = :memID; 
 		
-
 	-- DELETE
 		DELETE FROM Members WHERE memberID = :memID;
     
@@ -46,22 +44,31 @@
 
 	-- SELECT
 		-- select all albums 
-        
+        SELECT * FROM Albums;
         
 		-- select album from specific year 
+        SELECT Albums.albumID, Albums.albumName, Albums.artistID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias, Albums.year
+			FROM Albums
+            INNER JOIN Members ON Albums.artistID = Members.memberID
+            WHERE Albums.year = :userInput
+            ORDER BY Albums.albumName ASC;
         
+        -- select album specified by member ID
+        SELECT Albums.albumID, Albums.albumName, Albums.artistID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias, Albums.year
+			FROM Albums
+            INNER JOIN Members ON Albums.artistID = Members.memberID
+            WHERE Albums.artistID = :dropDownMenu
+            ORDER BY Albums.year;
         
-        -- select album specified by member
-        
-        
-        -- select album from multiple members
-        
-        
-        -- select albums including members named ''
-		
+        -- select album from mult members names, alias, album name, year
+        SELECT Albums.albumID, Albums.albumName, Albums.artistID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias, Albums.year
+			FROM Albums
+            INNER JOIN Members ON Albums.artistID = Members.memberID
+            WHERE (Members.lastName = "kim" OR Members.firstName = "" OR Members.alias = "" OR Albums.albumName = "")
+            ORDER BY Albums.albumName;
             
 	-- UPDATE
-		UPDATE Albums SET albumName = :albumName, artistID = :singer, year = :year;
+		UPDATE Albums SET albumName = :albumName, artistID = :singer, year = :year WHERE Albums.albumID = :input;
 
 	-- DELETE
 		DELETE FROM Albums WHERE albumID = :albumID;
@@ -75,17 +82,18 @@
 
 	-- SELECT
 		-- select all platforms
-		
+		SELECT * FROM Platforms;
             
 	-- UPDATE
 		-- update all parts of platform
 		UPDATE Platforms SET name = :platformName, numVideos = :num vids;
         
-        -- update number of vids by x amnt
+        -- update number of vids by x (for when a new video was added +1 or a video was removed -1)
+        UPDATE Platforms SET numVideos = numVideos -+ 1 WHERE platformID = :platformID;
         
 
 	-- DELETE
-		DELETE Platforms WHERE platformID = :platformID
+		DELETE FROM Platforms WHERE platformID = :platformID;
 	
     
 -- ----------------------------
@@ -97,12 +105,32 @@
 
 	-- SELECT
 		-- select all videos
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Platforms.name, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags
+			FROM Videos
+            INNER JOIN Platforms ON Videos.platformID = Platforms.platformID
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
         
         -- select videos by platform
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Platforms.name, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags
+			FROM Videos
+            INNER JOIN Platforms ON Videos.platformID = Platforms.platformID
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            WHERE Platforms.platformID = :platformID
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
         
         -- select videos by tags (act like categories)
-        
-        -- get all categories for a specific video
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags
+			FROM Videos
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            WHERE Tags.tag = :inputbydropdown
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
         
         -- select all videos that contain the phrase ''
         
@@ -143,6 +171,7 @@
 			VALUES	(:vidID, :tagID);
 
 	-- SELECT
+		-- select all tags
 		
             
 	-- UPDATE
@@ -159,7 +188,9 @@
 			VALUES	(:vidID, :memberID)
 
 	-- SELECT
+		-- select all artists from a video 
 		
+        -- VideoTags.tagID, Tags.tag
             
 	-- UPDATE
 		
