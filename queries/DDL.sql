3acde6cfd9bc0e72022b08fc821027ef11fa3438
Daisy <ramired2@oregonstate.edu>
2023-06-28 17:21:32 -0700

    moved folders

---
+++ queries/DDL.sql
@@ -0,0 +1,135 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+DROP TABLE IF EXISTS Members;
+DROP TABLE IF EXISTS Albums;
+DROP TABLE IF EXISTS Starring;
+DROP TABLE IF EXISTS Videos;
+DROP TABLE IF EXISTS Platforms;
+DROP TABLE IF EXISTS Tags;
+DROP TABLE IF EXISTS VideoTags;
+
+-- Table Creations
+
+CREATE TABLE Members (
+	memberID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    koreanName VARCHAR(45) NOT NULL,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    alias VARCHAR(45) NOT NULL default "No known alias",
+    PRIMARY KEY (memberID)
+);
+
+CREATE TABLE Albums (
+	albumID INT AUTO_INCREMENT NOT NULL,
+    albumName VARCHAR(45),
+    artistID INT NOT NULL,
+    year int,
+    PRIMARY KEY (albumID),
+    FOREIGN KEY (artistID) REFERENCES Members(memberID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Starring (
+	starringID INT AUTO_INCREMENT NOT NULL,
+	videoID INT,
+	artistID INT,
+    PRIMARY KEY (starringID),
+    FOREIGN KEY (videoID) REFERENCES Videos(videoID) ON UPDATE CASCADE,
+    FOREIGN KEY (artistID) REFERENCES Members(memberID) ON UPDATE CASCADE
+);
+
+CREATE TABLE Videos (
+	videoID INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(200),
+    description MEDIUMTEXT,
+    platformID INT NOT NULL,
+    link VARCHAR(255),
+    released date,
+    PRIMARY KEY (videoID),
+    FOREIGN KEY (platformID) REFERENCES Platforms(platformID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Platforms (
+	platformID INT AUTO_INCREMENT,
+	name VARCHAR(45) NOT NULL,
+    numVideos INT default 0,
+    PRIMARY KEY (platformID)
+);
+
+CREATE TABLE Tags (
+    tagID INT AUTO_INCREMENT NOT NULL,
+    tag VARCHAR (45) NOT NULL,
+    PRIMARY KEY (tagID)
+);
+
+CREATE TABLE VideoTags (
+	videoTagsID INT AUTO_INCREMENT NOT NULL,
+    videoID INT NOT NULL,
+    tagID INT NOT NULL,
+    PRIMARY KEY (videoTagsID),
+    FOREIGN KEY (videoID) REFERENCES Videos(videoID)  ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (tagID) REFERENCES Tags(tagID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- Data insertion
+
+INSERT INTO members(koreanName, lastName, firstName, alias)
+VALUES	("김남준" , "Kim", "Namjoon", "RM"),
+		("김석진", "Kim", "Seokjin", "Jin"),
+        ("민윤기", "Min", "Yoongi", "Suga"),
+        ("정호석", "Jung", "Hoseok", "JHope"),
+        ("박지민", "Park", "Jimin", "Jimin"),
+        ("김태형", "Kim", "Taehyung", "V"),
+        ("전정국", "Jeon", "Jungkook", "Jungkook"),
+        ("방탄소년단", "Bangtan", "Boys", "BTS"),
+        ("민윤기", "Min", "Yoongi", "AgustD"),
+        ("전정국", "Jeon", "Jungkook", "jk");
+        -- -- ("정호석", "Jung", "Hoseok", "J-Hope")
+        
+        
+INSERT INTO Albums (albumName, artistID, year)
+VALUES	("Mono", 1, 2018),
+		("D-2", 3, 2020),
+        ("Map of the Soul Persona", 8, 2019),
+        ("Jack in the Box", 4, 2022);
+        
+INSERT INTO Platforms (name, numVideos)
+VALUES	("Youtube", 3),
+		("Weverse", 0);
+        
+INSERT INTO Videos (name,
+					description,
+					platformID,
+					link)
+VALUES	("Run BTS! 2015 EP.1 - Open", "First episode of Run BTS!", 1, "https://www.youtube.com/watch?v=5XisXVcbZEw&list=PL5hrGMysD_Gut5B6Mms1bpkv44LoOwEHz"),
+		("Run BTS! 2015 EP.2 - 최고의 남자", "", 1, "https://www.youtube.com/watch?v=fJa1DMVB0vc&list=PL5hrGMysD_Gut5B6Mms1bpkv44LoOwEHz&index=2"),
+        ("Run BTS! 2015 EP.3 - Theme Park", "BTS goes to a themepark", 1, "https://www.youtube.com/watch?v=HMG7jim0Bqc&list=PL5hrGMysD_Gut5B6Mms1bpkv44LoOwEHz&index=3"),
+        ("161227 밥 먹는 김석진", "", 1, "https://www.youtube.com/watch?v=7fszwWLFAng");
+        
+INSERT INTO Tags (tag)
+VALUES	("Run BTS!"),
+		("Live"),
+        ("Comedy"),
+        ("Thriller"),
+        ("EAT Jin");
+        
+INSERT INTO VideoTags (videoID, tagID)
+VALUES	(1, 1),
+		(1, 3),
+		(2, 3),
+        (3, 4),
+        (4, 5);
+        
+INSERT INTO Starring (videoID, artistID)
+VALUES	( 1, 3),
+		( 1, 1),
+        ( 1, 5),
+		( 2, 7),
+        ( 2, 3),
+        ( 2, 6),
+        ( 2, 1),
+        ( 4, 2);
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
---
+++ queries/DML.sql
@@ -0,0 +1,264 @@
+-- ----------------------------
+-- 		MEMBERS
+-- ----------------------------
+	-- INSERT
+        INSERT INTO members(koreanName, lastName, firstName, alias)
+			VALUES	(:kn , :ln, :fn, :alias);
+
+	-- SELECT -- unless specified order by their fanchant 
+		-- select all members
+        SELECT * FROM Members;
+        
+        -- select all members w out other known aliases (like suga, agustD, prodD)
+		SELECT Members.memberID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias 
+			FROM Members GROUP BY fullName;
+        
+		-- select members by specified last name OR first name (w out other known aliases)
+       SELECT Members.memberID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName , Members.alias 
+			FROM Members 
+            WHERE (Members.lastName = :"" OR Members.firstName = :"") 
+            GROUP BY fullName;
+        
+        -- select members by ID
+        SELECT Members.memberID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias 
+			FROM Members 
+            WHERE Members.memberID = :fromDropdown
+        
+        -- select all people starring in a video
+        SELECT Starring.videoID, Starring.artistID, Members.memberID, Members.alias, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.lastName as ln
+			FROM Starring 
+            INNER JOIN Videos ON Starring.videoID = Videos.videoID
+            INNER JOIN Members ON Starring.artistID = Members.memberID
+            WHERE Videos.videoID = :videoID
+            ORDER BY Members.lastName ASC;
+            
+	-- UPDATE
+		UPDATE members SET koreanName = :kn, lastName = :ln, firstName = :fn, alias = :alias WHERE memberID = :memID; 
+		
+	-- DELETE
+		DELETE FROM Members WHERE memberID = :memID;
+    
+-- ----------------------------
+-- 		ALBUMS
+-- ----------------------------
+	-- INSERT
+		INSERT INTO Albums (albumName, artistID, year)
+			VALUES	(:album, :singer, :year);
+
+	-- SELECT
+		-- select all albums 
+        SELECT * FROM Albums;
+        
+		-- select album from specific year 
+        SELECT Albums.albumID, Albums.albumName, Albums.artistID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias, Albums.year
+			FROM Albums
+            INNER JOIN Members ON Albums.artistID = Members.memberID
+            WHERE Albums.year = :userInput
+            ORDER BY Albums.albumName ASC;
+        
+        -- select album specified by member ID
+        SELECT Albums.albumID, Albums.albumName, Albums.artistID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias, Albums.year
+			FROM Albums
+            INNER JOIN Members ON Albums.artistID = Members.memberID
+            WHERE Albums.artistID = :dropDownMenu
+            ORDER BY Albums.year;
+        
+        -- select album from mult members names, alias, album name, year
+        SELECT Albums.albumID, Albums.albumName, Albums.artistID, CONCAT(Members.lastName, " ", Members.firstName) AS fullName, Members.alias, Albums.year
+			FROM Albums
+            INNER JOIN Members ON Albums.artistID = Members.memberID
+            WHERE (Members.lastName = :"" OR Members.firstName = :"" OR Members.alias = :"" OR Albums.albumName = :"")
+            ORDER BY Albums.albumName;
+            
+	-- UPDATE
+		UPDATE Albums SET albumName = :albumName, artistID = :singer, year = :year WHERE Albums.albumID = :input;
+
+	-- DELETE
+		DELETE FROM Albums WHERE albumID = :albumID;
+
+-- ----------------------------
+-- 		PLATFORMS
+-- ----------------------------
+	-- INSERT
+		INSERT INTO Platforms (name, numVideos)
+			VALUES	(:name, :numVid);
+
+	-- SELECT
+		-- select all platforms
+		SELECT * FROM Platforms;
+            
+	-- UPDATE
+		-- update all parts of platform
+		UPDATE Platforms SET name = :platformName, numVideos = :num vids;
+        
+        -- update number of vids by x (for when a new video was added +1 or a video was removed -1)
+        UPDATE Platforms SET numVideos = numVideos -+ 1 WHERE platformID = :platformID;
+        
+
+	-- DELETE
+		DELETE FROM Platforms WHERE platformID = :platformID;
+	
+    
+-- ----------------------------
+-- 		VIDEOS
+-- ----------------------------
+	-- INSERT
+		INSERT INTO Videos (name, description, platformID, link)
+			VALUES	(:vidName, :desc, :platformID, :link);
+
+	-- SELECT
+		-- select all videos
+        SELECT * FROM Videos;
+        
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Platforms.name, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, 
+		GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags, GROUP_CONCAT(DISTINCT Members.alias SEPARATOR ', ') as starring
+			FROM Videos
+            INNER JOIN Platforms ON Videos.platformID = Platforms.platformID
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            LEFT JOIN Starring ON Videos.videoID = Starring.videoID
+            LEFT JOIN Members ON Starring.artistID = Members.memberID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
+        
+        -- select videos by platform
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Platforms.name, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, 
+        GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags, GROUP_CONCAT(DISTINCT Members.alias SEPARATOR ', ') as starring
+			FROM Videos
+            INNER JOIN Platforms ON Videos.platformID = Platforms.platformID
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            LEFT JOIN Starring ON Videos.videoID = Starring.videoID
+            LEFT JOIN Members ON Starring.artistID = Members.memberID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            WHERE Platforms.platformID = :platformID
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
+        
+        -- select videos by tags (act like categories)
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, 
+        GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags, GROUP_CONCAT(DISTINCT Members.alias SEPARATOR ', ') as starring
+			FROM Videos
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            LEFT JOIN Starring ON Videos.videoID = Starring.videoID
+            LEFT JOIN Members ON Starring.artistID = Members.memberID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            WHERE Tags.tag = :inputbydropdown
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
+        
+        -- select all videos that contain the phrase '' in the name or in the tags // helpful for gen search 
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, 
+        GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags, GROUP_CONCAT(DISTINCT Members.alias SEPARATOR ', ') as starring
+			FROM Videos
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            LEFT JOIN Starring ON Videos.videoID = Starring.videoID
+            LEFT JOIN Members ON Starring.artistID = Members.memberID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            WHERE Videos.name LIKE :'%%' OR Tags.tag LIKE :'%%' OR Members.alias LIKE :'%%'-- quotes r same
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
+        
+        -- select videos that contain specific member // NOT FULL STARRING RETURNED 
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Videos.link, Videos.videoID as videoContained, 
+        GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags, 
+				(SELECT GROUP_CONCAT(DISTINCT Members.alias SEPARATOR ', ') as starring 
+				FROM MEMBERS 
+                LEFT JOIN Starring ON Starring.artistID = Members.memberID 
+                INNER JOIN Videos ON Starring.videoID = Videos.videoID 
+                WHERE Videos.videoID = videoContained) as starred
+			FROM Videos
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            LEFT JOIN Starring ON Videos.videoID = Starring.videoID
+            LEFT JOIN Members ON Starring.artistID = Members.memberID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            WHERE members.memberID = :memberID -- might have to add name or alias feature here too 
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
+        
+        -- select four random videos for opening page by videoID
+        SELECT Videos.videoID, Videos.name, Videos.description, Videos.platformID, Platforms.name, Videos.link, GROUP_CONCAT(DISTINCT Tags.tagID ORDER BY Tags.tagID ASC) as tagID, 
+		GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags, GROUP_CONCAT(DISTINCT Members.alias SEPARATOR ', ') as starring
+			FROM Videos
+            INNER JOIN Platforms ON Videos.platformID = Platforms.platformID
+            INNER JOIN VideoTags ON Videos.videoID = VideoTags.videoID
+            LEFT JOIN Starring ON Videos.videoID = Starring.videoID
+            LEFT JOIN Members ON Starring.artistID = Members.memberID
+            INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+            WHERE Videos.videoID = :randNum OR Videos.videoID = :randNum OR Videos.videoID = :randNum OR Videos.videoID = :randNum
+            GROUP BY Videos.videoID
+            ORDER BY Videos.name;
+            
+	-- UPDATE
+		UPDATE Videos SET name = :name, description = :desc, platformID = (SELECT PlatformID FROM Platforms WHERE Platforms.name = :platformName), link = :link 
+			WHERE Videos.videoID = :vidId;
+        
+        -- manually insert platform name -- if id rem select and just put id from dropdown
+		UPDATE Videos SET platformID = (SELECT PlatformID FROM Platforms WHERE Platforms.name = :platformName) WHERE Videos.videoID = :vidID;
+
+	-- DELETE
+	DELETE FROM Videos WHERE Videos.videoID = :videoID
+    
+-- ----------------------------
+-- 		TAGS
+-- ----------------------------
+	-- INSERT
+		INSERT INTO Tags (name)
+			VALUES	(:tag)
+
+	-- SELECT
+		SELECT * FROM Tags
+            
+	-- UPDATE
+		UPDATE Tags SET tag = :tag;
+
+	-- DELETE
+	DELETE FROM Tags WHERE tagID = :tagID
+    
+-- ----------------------------
+-- 		VIDEO TAGS
+-- ----------------------------
+	-- INSERT
+		INSERT INTO VideoTags (videoID, tagID)
+			VALUES	(:vidID, :tagID);
+
+	-- SELECT
+		-- select all
+        SELECT * FROM VideoTags;
+        
+        SELECT VideoTags.videoID, Videos.name, GROUP_CONCAT(DISTINCT Tags.tag ORDER BY Tags.tag ASC SEPARATOR ', ') as tags
+			FROM VideoTags
+			INNER JOIN Tags ON VideoTags.tagID = Tags.tagID
+			INNER JOIN Videos ON VideoTags.videoID = Videos.videoID
+			GROUP BY VideoTags.videoID
+			ORDER BY Videos.Name;
+            
+	-- UPDATE
+		-- no need for update can just delete relation
+
+	-- DELETE
+	DELETE FROM VideoTags WHERE videoTagsID = :videotagID
+    
+-- ----------------------------
+-- 		STARRING
+-- ----------------------------
+	-- INSERT
+		INSERT INTO Starring (videoID, artistID)
+			VALUES	(:vidID, :memberID);
+
+	-- SELECT
+		SELECT * FROM Starring;
+        
+		-- select all artists from a video by id
+        SELECT Videos.videoID, Members.alias as starring, CONCAT(Members.lastName, ' ', Members.firstName) as fullName
+				FROM MEMBERS 
+                LEFT JOIN Starring ON Starring.artistID = Members.memberID 
+                INNER JOIN Videos ON Starring.videoID = Videos.videoID 
+                WHERE Videos.videoID = :vidID;
+            
+	-- UPDATE 
+		UPDATE Starring SET artistID = :artistID  WHERE videoID = :videoIn;
+        UPDATE Starring SET videoID = :videoIn WHERE artistID = :artistID;
+
+	-- DELETE
+	DELETE FROM Starring WHERE starringID = :starringID;
+    
