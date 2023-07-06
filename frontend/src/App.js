fb08ebf44ebd09933bf9974b1426170a5a51f85d
Daisy <ramired2@oregonstate.edu>
2023-07-05 22:02:22 -0700

    styling homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+
+import Homepage from './pages/Homepage.js';
+
+/* special library and its components to perform redirection easily */
+import {
+  BrowserRouter as Router, // store the components and its routes as an object
+  Route, // a statement that holds the specific path of the app and the component's name, renders it once it matches the URL
+  Routes
+} from "react-router-dom"; // more about that here: https://www.pluralsight.com/guides/how-to-set-react-router-default-route-redirect-to-home
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,56 @@
+// import React, {useState, useEffect} from 'react';
+
+const Homepage = () => {
+
+  return(
+  <div className="content">
+
+    {/* not done */}
+    <div className="2kool4school album"></div>
+    <div className="orul82 album"></div>
+    <div className="skoolluvaffair album"></div>
+    <div className="dark&wild album"></div>
+    <div className="hyyh1 album"></div>
+    <div className="hyyh2 album"></div>
+    <div className="youngforever album"></div>
+    <div className="wings album"></div>
+    <div className="neverwalkalone album"></div>
+
+    <div className="armybomb">
+      <div className="armyBase"></div>
+      <div className="armyBody"></div>
+    </div>
+
+    <div className="her album"></div>
+    <div className="tear album"></div>
+    <div className="answer album"></div>
+
+    <div className="persona album"></div>
+    <div className="7 album"></div>
+    <div className="be album"></div>
+    <div className="proof album"></div>
+    {/* not done end */}
+
+
+    <div className="shelfEdge topShelf"></div>
+    <div className="shelfShadow topShelfShadow"></div>
+
+    <div className="vert vertLineL"></div>
+    <div className="vert vertLineR"></div>
+
+    <div className="shelfShadow bottomShelfShadow"></div>
+    <div className="shelfEdge bottomShelf"></div>
+
+    <div className="house">
+      <div className="houseRoofL roof"></div>
+      <div className="houseRoofR roof"></div>
+      <div className="houseBody"></div>
+    </div>
+    
+    
+  </div>
+  );
+}
+
+
+export default Homepage;
