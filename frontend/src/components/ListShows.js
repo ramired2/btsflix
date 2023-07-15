47fa8e16535d08f06efdb1c4dea270a310abe05b
Daisy <ramired2@oregonstate.edu>
2023-07-15 16:48:24 -0700

    fixed modal

--- frontend/src/components/ListShows.js
+++ frontend/src/components/ListShows.js
@@ -1,8 +1,30 @@
 import React, {useState} from "react";
-import ModalShow from "./ModalShow";
+// import ModalShow from "./ModalShow";
 
 function ListShows({shows, category}){
     const [Hover, setHover] = useState(false);
+
+    const modal = () => {
+        return <div className="modalContainer" 
+        >
+            <div className="closeBtn" onClick={() => setHover(false)}>x</div>
+        <div className="infoContainer">
+            <h1 className="videoName">{Hover.name}</h1>
+            <iframe width="79%" height="315" className="video"
+                src={Hover.link}>
+            </iframe>
+            <div className="contentInfo">
+                <ol className="text infoList desc">
+                    <li>{Hover.description}</li>
+                </ol>
+                <ol className="infoList details">
+                    <li className="indivItem">Category: {Hover.tags}</li>
+                    <li>Cast: {Hover.starring}</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    }
     
     return(
         <div>
@@ -12,7 +34,7 @@ function ListShows({shows, category}){
                 <div className="scrolling">
                     {shows.map ((show, idx) => 
                         <div className="indivShow" onClick={() => {setHover(show);}}
-                                                   onMouseLeave={() => setHover(false)}
+                                                //    onMouseLeave={() => setHover(false)}
                                                    >
                             {console.log("hovered on ", Hover)}
                             {/* {Hover != false? <ModalShow info={Hover}></ModalShow>:""} */}
@@ -20,7 +42,7 @@ function ListShows({shows, category}){
                         {/* {Hover != false? <ModalShow info={Hover}></ModalShow>:""} */}
                 </div>
             </div>
-            {Hover != false? <ModalShow info={Hover}></ModalShow>:""}
+            {Hover != false? modal():""}
         </div>
     );
 }
