998eb8acbc7f727684ce04ba1b2e3a6c62f74373
Daisy Ramirez <72051193+ramired2@users.noreply.github.com>
2023-08-09 12:28:12 -0700

    rem db password

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const mysql = require("mysql2")
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "8432Befly",
+    password: "",
     database: "btsflix"
 });
 
